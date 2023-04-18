2e5953ae5d300ee5db138b87305fd3ff8682a966
Maxim Fomov <maksimfomov26@gmail.com>
2023-04-18 12:05:02 +0300

    feat: Updated User and Userdetail models and added application.properties file for MySql

--- src/main/java/com/fomov/kanbanboard/model/User.java
+++ src/main/java/com/fomov/kanbanboard/model/User.java
@@ -2,6 +2,8 @@ package com.fomov.kanbanboard.model;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -20,4 +22,58 @@ public class User {
             cascade = CascadeType.ALL)
     @JoinColumn(name = "user_detail_id", nullable = false)
     private UserDetail userDetail;
+
+    public User() {}
+
+    public User(int id, String email, String password, UserDetail userDetail) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.userDetail = userDetail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserDetail getUserDetail() {
+        return userDetail;
+    }
+
+    public void setUserDetail(UserDetail userDetail) {
+        this.userDetail = userDetail;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(userDetail, user.userDetail);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password, userDetail);
+    }
 }
--- src/main/java/com/fomov/kanbanboard/model/UserDetail.java
+++ src/main/java/com/fomov/kanbanboard/model/UserDetail.java
@@ -2,6 +2,8 @@ package com.fomov.kanbanboard.model;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "users_details")
 public class UserDetail {
@@ -24,4 +26,67 @@ public class UserDetail {
             cascade = {CascadeType.PERSIST, CascadeType.MERGE,
                     CascadeType.DETACH, CascadeType.REFRESH})
     private User user;
+
+    public UserDetail() {}
+
+    public UserDetail(int id, String firstName, String lastName, String position, User user) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDetail that = (UserDetail) o;
+        return id == that.id && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(position, that.position) && Objects.equals(user, that.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, position, user);
+    }
 }
